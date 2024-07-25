f97daffb0b77bf7f86f03a1a91b06cd13c5db153
Murilo Guizelin <murilomotta4@gmail.com>
2024-07-25 14:52:19 -0400

    kart indoor exercise eimp

--- Maratona  EIMP/K.cpp
+++ Maratona  EIMP/K.cpp
@@ -4,5 +4,26 @@ using namespace std;
 
 //usou buble sort, ou mergesort, usando swap do cpp
 int main(){
-    
+    int N;
+    cin >> N;
+    // cria um vetor de tamanho N
+    vector<int> v(N);
+
+    // le as posições de chegada
+    for(int i = 0; i < N; i++){
+        cin >> v[i];
+    }
+    int ans = 0;
+
+
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++){
+            if(v[i] > v[j]){
+                // troca os valores
+                swap(v[i], v[j]);
+                ans++;
+            }
+        }
+    }
+    cout << ans << endl;
 }
