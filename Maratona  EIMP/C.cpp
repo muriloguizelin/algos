40e6a89f04eac26873b5ba13d34a95987e28f08b
Murilo Guizelin <murilomotta4@gmail.com>
2024-07-25 14:29:36 -0400

    Exercicio C da maratona

--- Maratona  EIMP/C.cpp
+++ Maratona  EIMP/C.cpp
@@ -2,10 +2,26 @@
 
 using namespace std;
 
-//passa a matriz e verfiica se a transposta dela é simetrica
 int main(){
  int n;
  cin >> n;
 
+vector<vector<char>> matriz(n, vector<char>(n, 0));
 
+for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+        cin >> matriz[i][j];
+    }
+}
+
+bool simetrica = true;
+for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+        if(matriz[i][j] != matriz[j][i]){
+            simetrica = false;
+            break;
+        }
+    }
+}
+    cout << (simetrica ? "Sim" : "Nao") << endl;
 }
